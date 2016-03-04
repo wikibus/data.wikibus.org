c3003558257ab1123632d2d80a0f0100700f61cb
tpluscode <tpluscode@users.noreply.github.com>
2016-03-04 07:22:49 +0100

    init

---
+++ HydraStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Nancy.Bootstrapper;
+using Nancy.Hydra;
+using wikibus.common;
+
+namespace data.wikibus.org
+{
+    /// <summary>
+    /// Starts Hydra
+    /// </summary>
+    public class HydraStartup : IApplicationStartup
+    {
+        private readonly IWikibusConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HydraStartup"/> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public HydraStartup(IWikibusConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// Perform any initialization tasks
+        /// </summary>
+        public void Initialize(IPipelines pipelines)
+        {
+            var baseUri = new Uri(_config.BaseApiNamespace);
+            pipelines.UseHydra(new Uri(baseUri, "doc"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace data.wikibus.org
+{
+    internal class Program
+    {
+        private const int DefaultPort = 17899;
+
+        private static void Main(string[] args)
+        {
+            int port = DefaultPort;
+            if (args.Length > 0)
+            {
+                int.TryParse(args[0], out port);
+            }
+
+            var urlBuilder = new UriBuilder("http://localhost")
+            {
+                Port = port
+            };
+
+            using (WebApp.Start<Startup>(urlBuilder.ToString()))
+            {
+                Console.WriteLine("started nancy");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.Use(async (context, func) =>
+            {
+                Console.WriteLine(context.Request.Uri);
+                await func();
+            });
+            builder.UseNancy();
+        }
+    }
+}

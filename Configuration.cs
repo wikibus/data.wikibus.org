75a614434310d5353da850248633b60b9538ffe5
Tomasz Pluskiewicz <tpluskiewicz@pgs-soft.com>
2016-03-04 16:05:17 +0100

    sql connection string from env

---
+++ Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using wikibus.sources.dotNetRDF;
+
+namespace data.wikibus.org
+{
+    public class Configuration : ISourcesDatabaseConnectionStringProvider
+    {
+        public string ConnectionString
+        {
+            get { return Environment.GetEnvironmentVariable("wikibus-sql-connectionstring"); }
+        }
+    }
+}

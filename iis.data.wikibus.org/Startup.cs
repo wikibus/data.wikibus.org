80aef55183300d709f19677530ad69b7969759a5
tpluscode <tpluscode@users.noreply.github.com>
2016-03-05 10:35:46 +0100

    add web project to solution

---
+++ iis.data.wikibus.org/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using data.wikibus.org;
+using Microsoft.Owin;
+using Nancy;
+using Owin;
+using wikibus.nancy;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace data.wikibus.org
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.Use(async (context, func) =>
+            {
+                Console.WriteLine(context.Request.Uri);
+                await func();
+            });
+
+#if DEBUG
+            StaticConfiguration.DisableErrorTraces = false;
+#endif
+            builder.UseNancy(options => options.Bootstrapper = new Bootstrapper());
+        }
+    }
+}

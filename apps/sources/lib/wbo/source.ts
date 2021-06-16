7b0a045e90c3c8933a1961d8574129496d5c8264
tpluscode <tpluscode@users.noreply.github.com>
2021-06-16 08:48:56 +0200

    fix: slugify title

--- apps/sources/lib/wbo/source.ts
+++ apps/sources/lib/wbo/source.ts
@@ -3,6 +3,9 @@ import { schema } from '@tpluscode/rdf-ns-builders/strict'
 import clownface from 'clownface'
 import $rdf from 'rdf-ext'
 import { INSERT } from '@tpluscode/sparql-builder'
+import slugify from 'slugify'
+import { nanoid } from 'nanoid'
+import type { TransformVariable } from '@hydrofoil/knossos/collection'
 
 export const initializeImagesResource: Handler = async ({ event, req }) => {
   if (!event.object) {
@@ -19,4 +22,8 @@ export const initializeImagesResource: Handler = async ({ event, req }) => {
   await INSERT.DATA`GRAPH ${event.object.id} {
         ${event.object.id} ${schema.associatedMedia} ${images.term}
     }`.execute(req.labyrinth.sparql.query)
+}
+
+export const slugifyTitle: TransformVariable = (term) => {
+  return $rdf.literal(`${slugify(term.value, { lower: true })}-${nanoid(6)}`)
 }

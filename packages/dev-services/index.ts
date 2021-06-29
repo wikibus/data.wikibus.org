b962d5753e9952ee826465f93e72f52a20271877
tpluscode <tpluscode@users.noreply.github.com>
2021-06-29 20:25:11 +0200

    ci: added a mocked image service

--- packages/dev-services/index.ts
+++ packages/dev-services/index.ts
@@ -1,9 +1,25 @@
 import type { NamedNode } from '@rdfjs/types'
-import { FileStorage } from '@wikibus/core'
+import { FileStorage, ImageStorage, UploadedImage } from '@wikibus/core'
 import $rdf from 'rdf-ext'
+import { nanoid } from 'nanoid'
 
 export class FileService implements FileStorage {
   async upload(name: string): Promise<NamedNode> {
     return $rdf.namedNode(`file:${name}`)
+  }
+}
+
+export class ImageService extends ImageStorage {
+  async deleteImage(): Promise<void> {
+    return undefined
+  }
+
+  async upload(): Promise<UploadedImage> {
+    const id = nanoid()
+    return {
+      id,
+      thumbnailUrl: `/image/${id}/thumb`,
+      contentUrl: `/image/${id}`,
+    }
   }
 }
